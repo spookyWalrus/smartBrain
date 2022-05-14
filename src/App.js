d1accebfa77343c48ce3e8e23b8c2aa3eef5b459
Zen Nakamura <zenzenzen@gmail.com>
2022-05-14 16:52:06 -0400

    deploy

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ class App extends Component {
     }
 
   afterAPIcall = () =>{ // increment counter
-    fetch('https://polar-stream-61468.herokuapp.com:3000/image', {
+    fetch('https://polar-stream-61468.herokuapp.com/image', {
         method: 'put',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
