0e56d5b0b5aaf5d739afd85b233b46df84daee37
Zen Nakamura <zenzenzen@gmail.com>
2022-12-02 11:47:58 -0500

    added eslint comment due to build error

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -41,7 +41,8 @@ class Register extends React.Component {
 	}
 
 	render() {
-		const {onRouteChange} = this.props; 
+		const {onRouteChange} = this.props; // eslint-disable-line no-unused-vars
+
 
 		return(
 			<article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
