e75512dae4303d73b8ebaa414a42c5027463ff59
Zen Nakamura <zenzenzen@gmail.com>
2022-12-02 10:56:11 -0500

    deleted eslint comment

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -41,7 +41,7 @@ class Register extends React.Component {
 	}
 
 	render() {
-		const {onRouteChange} = this.props; // eslint-disable-line no-unused-vars
+		const {onRouteChange} = this.props; 
 
 		return(
 			<article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
